952bdb1bfbf187b4b6e00608665bb4d81484dcc5
Brendan de Faria <brendan@Brendans-MacBook-Air.local>
2021-05-21 03:34:04 +0200

    first commit

---
+++ lib/colors/app_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final Color babygymPrimary = Color(0xFF36474b);
+  static final Color babygymSecondary = Color(0xFF54676a);
+  static final Color babygymGrey = Color(0xFFdae6e8);
+  static final Color babygymBlue = Color(0xFFadd8e6);
+  static final Color babygymWhite = Color(0xFFFFFEFC);
+}
