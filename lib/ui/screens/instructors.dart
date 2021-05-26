ed31fc6c8375021c716ca3044f39860fb5d76c73
Brendan de Faria <brendan@Brendans-MacBook-Air.local>
2021-05-26 11:02:59 +0200

    Instructors page list working

--- lib/ui/screens/instructors.dart
+++ lib/ui/screens/instructors.dart
@@ -1,15 +1,82 @@
-import 'package:babygym/firebase/flutterfire.dart';
+import 'package:babygym/colors/app_theme.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Instructors extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        child: MaterialButton(
-      onPressed: () {
-        addInstructors();
-      },
-      child: Text('get display name'),
-    ));
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      decoration: BoxDecoration(color: AppTheme.babygymSecondary),
+      child: Center(
+        child: StreamBuilder(
+          stream:
+              FirebaseFirestore.instance.collection('Instructors').snapshots(),
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (!snapshot.hasData) {
+              return Center(
+                child: CircularProgressIndicator(
+                  color: Colors.white,
+                ),
+              );
+            }
+            return ListView(
+              children: snapshot.data!.docs.map((document) {
+                return Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 7, horizontal: 15),
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                    width: MediaQuery.of(context).size.width / 1.3,
+                    // height: MediaQuery.of(context).size.height / 12,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: AppTheme.babygymWhite,
+                    ),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            '${(document.data() as dynamic)['name']}',
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: AppTheme.babygymPrimary,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Text(
+                            "${(document.data() as dynamic)['city']}",
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: AppTheme.babygymPrimary,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Text(
+                            "Date: ${(document.data() as dynamic)['email']}",
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: AppTheme.babygymPrimary,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              }).toList(),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
