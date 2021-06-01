91e83c1cad2a9a042093b87916ca0d0595fb0f1a
Brendan de Faria <brendan@Brendans-MacBook-Air.local>
2021-06-01 16:56:22 +0200

    set up firebase hosting with github updates

--- lib/colors/app_theme.dart
+++ lib/colors/app_theme.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static final Color babygymPrimary = Color(0xFF36474b);
+  // static final Color babygymPrimary = Color(0xFF36474b);
+  static final Color babygymPrimary = Color(0xFF301934);
   static final Color babygymSecondary = Color(0xFF54676a);
   static final Color babygymGrey = Color(0xFFdae6e8);
   static final Color babygymBlue = Color(0xFFadd8e6);
