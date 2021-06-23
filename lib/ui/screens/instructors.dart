70804435bf1921b62895233b84527b925751bf5f
Maurits Laubser <maurits.laubser@gmail.com>
2021-06-23 15:02:02 +0200

    added sorting feature for instuctors page

--- lib/ui/screens/instructors.dart
+++ lib/ui/screens/instructors.dart
@@ -1,31 +1,121 @@
+import 'dart:html';
+
+import 'package:babygym/cards/instuctorCard.dart';
 import 'package:babygym/colors/app_theme.dart';
 import 'package:babygym/ui/screens/add_apointment.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:babygym/model/instructor.dart';
 
-class Instructors extends StatelessWidget {
+class Instructors extends StatefulWidget {
+  @override
+  _Instructors createState() => _Instructors();
+}
+
+class _Instructors extends State<Instructors> {
+  bool typing = false;
+  TextEditingController _searchController = TextEditingController();
+  late Future resultsLoaded;
+  List _allResults = [];
+  List _resultsList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _searchController.addListener(_onSearchChanged);
+  }
+
+  @override
+  void dispose() {
+    _searchController.removeListener(_onSearchChanged());
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    resultsLoaded = getInstructors();
+  }
+
+  _onSearchChanged() {
+    searchResultsList();
+  }
+
+  searchResultsList() {
+    var showResults = [];
+    if (_searchController.text != "") {
+      for (var instSanp in _allResults) {
+        var name = Instructor.fromSnapshot(instSanp).name.toLowerCase();
+        var city = Instructor.fromSnapshot(instSanp).city.toLowerCase();
+        if (name.contains(_searchController.text.toLowerCase()) ||
+            city.contains(_searchController.text.toLowerCase())) {
+          showResults.add(instSanp);
+        }
+      }
+    } else {
+      showResults = List.from(_allResults);
+    }
+    setState(() {
+      _resultsList = showResults;
+    });
+  }
+
+  getInstructors() async {
+    var items =
+        await FirebaseFirestore.instance.collection('Instructors').get();
+    setState(() {
+      _allResults = items.docs;
+    });
+    searchResultsList();
+    return "Complete";
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Instructors'),
+        title: typing
+            ? Center(
+                child: Container(
+                  alignment: Alignment.centerLeft,
+                  color: AppTheme.babygymPrimary,
+                  child: TextField(
+                    controller: _searchController,
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Search',
+                      labelStyle: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              )
+            : Center(child: Text("Instructors")),
         backgroundColor: AppTheme.babygymPrimary,
-        actions: [TextField(
-          decoration: InputDecoration(
-            hintText: 'Search', 
-            prefixIcon: Icon(Icons.search)),
-        )],
-        
+        leading: IconButton(
+          icon: Icon(typing ? Icons.done : Icons.search),
+          onPressed: () {
+            setState(() {
+              typing = !typing;
+              _searchController.clear();
+            });
+          },
+        ),
       ),
       body: Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
         decoration: BoxDecoration(color: AppTheme.babygymSecondary),
         child: Center(
-          child: StreamBuilder(
-            stream: FirebaseFirestore.instance
-                .collection('Instructors')
-                .snapshots(),
+          child: ListView.builder(
+            itemCount: _resultsList.length,
+            itemBuilder: (BuildContext contex, int index) =>
+                buildInstructorCard(context, _resultsList[index]),
+          ),
+          /*
+          StreamBuilder(
+            stream: getInstructors(),
             builder:
                 (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
               if (!snapshot.hasData) {
@@ -82,23 +172,14 @@ class Instructors extends StatelessWidget {
                                   Padding(
                                     padding: const EdgeInsets.all(8.0),
                                     child: Text(
-                                      "${(document.data() as dynamic)['name']} (" + "${(document.data() as dynamic)['city']})",
+                                      "${(document.data() as dynamic)['name']} (" +
+                                          "${(document.data() as dynamic)['city']})",
                                       style: TextStyle(
                                         fontSize: 20,
                                         color: AppTheme.babygymPrimary,
                                       ),
                                     ),
                                   ),
-                                  // Padding(
-                                  //   padding: const EdgeInsets.all(5.0),
-                                  //   child: Text(
-                                  //     "${(document.data() as dynamic)['city']}",
-                                  //     style: TextStyle(
-                                  //       fontSize: 12,
-                                  //       color: AppTheme.babygymPrimary,
-                                  //     ),
-                                  //   ),
-                                  // ),
                                   Padding(
                                     padding: const EdgeInsets.all(5.0),
                                     child: Text(
@@ -120,7 +201,7 @@ class Instructors extends StatelessWidget {
                 }).toList(),
               );
             },
-          ),
+          ),*/
         ),
       ),
     );
